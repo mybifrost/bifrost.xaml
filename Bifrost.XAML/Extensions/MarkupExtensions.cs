65fccfab5e07b147fe6cba6fd5414459f40a5d1f
Tony Sullivan <tony.f.sullivan@gmail.com>
2015-06-23 16:26:00 -0400

    Adding a markup extension for DependencyObject.GetParent<T>() to find an element's parent of a specific type (page, frame, grid, etc)

--- Bifrost.XAML/Extensions/MarkupExtensions.cs
+++ Bifrost.XAML/Extensions/MarkupExtensions.cs
@@ -6,6 +6,12 @@ namespace Bifrost.XAML.Extensions
 {
     public static class MarkupExtensions
     {
+        /// <summary>
+        /// Searches down the UI tree to find all anscestors of a given type.
+        /// </summary>
+        /// <typeparam name="T">Type of ancestors to be found.</typeparam>
+        /// <param name="root">Element to begin searching down from.</param>
+        /// <returns>List of all ancestors of the given type.  </returns>
         public static IEnumerable<T> GetAnscestors<T>(this DependencyObject root) where T : DependencyObject
         {
             var list = new List<T>();
@@ -21,6 +27,26 @@ namespace Bifrost.XAML.Extensions
             }
 
             return list;
+        }
+
+        /// <summary>
+        /// Finds the first parent of a given type.
+        /// </summary>
+        /// <typeparam name="T">Type of parent to be found.</typeparam>
+        /// <param name="root">Element to begin searching up from.</param>
+        /// <returns>First parent of given type, or null if no parents match.</returns>
+        public static T GetParent<T>(this DependencyObject root) where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(root);
+            if (parent != null)
+            {
+                if (parent is T)
+                    return parent as T;
+                else
+                    return GetParent<T>(parent);
+            }
+            else
+                return null;
         }
     }
 }
